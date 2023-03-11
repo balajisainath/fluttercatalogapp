ecc5da3065ed703df7758df5c734d23ba48b949e
balajisainath <sainathbalaji01@gmail.com>
2023-03-11 23:10:19 +0530

    day12 gridlayout

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -42,12 +42,41 @@ class _HomepageState extends State<Homepage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ?ListView.builder(
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ?GridView.builder(
           itemCount: CatalogModel.items.length,
-            itemBuilder: (context,index){
-              return ItemWidget(item: CatalogModel.items[index],);
-            }
-        ): Center(child:
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              mainAxisSpacing: 16, // this is spacing between the rows
+              crossAxisSpacing: 16,
+            ),
+          itemBuilder:(context,index){
+              final item=CatalogModel.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
+                  child: GridTile(
+                    header: Container(
+                      padding: EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                        ),
+                        child: Text(item.price.toString(),style: TextStyle(color: Colors.white),)
+                    ),
+                      child:Image.network(item.image),
+                      footer: Container(
+                          padding: EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                              color: Colors.black
+                          ),
+                          child: Text(item.name,style: TextStyle(color: Colors.white),)
+                      ),
+                  )
+              );
+        },
+
+        )
+            : Center(child:
           CircularProgressIndicator(),)
       ),
       drawer: MyDrawer(),
